badaeb0042f38ed8e5b6008b9aa7d61ab1341ecc
Geoff Ford <geoff.golang.12@gmail.com>
2025-08-07 17:43:58 +0100

    Updated comments

--- get_intra.go
+++ get_intra.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gford1000-go/alphav/intraday"
 )
 
+// GetIntradayData returns data for the specified symbol, using the api_key stored in the context.
+// opts allows the behaviour of the call to be varied per the options in https://www.alphavantage.co/documentation/
+// for TIME_SERIES_INTRADAY
 func GetIntradayData(ctx context.Context, symbol string, opts ...func(*intraday.Options) error) (*intraday.Data, error) {
 
 	apiKey, err := getAPIKey(ctx)
