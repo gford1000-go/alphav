b19d47687ede8c151e69f47f7e0ef3be72c73816
Geoff Ford <geoff.golang.12@gmail.com>
2025-08-27 10:03:29 +0100

    Corrected comment

--- get_fx.go
+++ get_fx.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gford1000-go/alphav/fx"
 )
 
-// GetIntradayData returns data for the specified currency pair, using the api_key stored in the context.
+// GetFX returns data for the specified currency pair, using the api_key stored in the context.
 // opts allows the behaviour of the call to be varied per the options in https://www.alphavantage.co/documentation/
 // for FX_DAILY
 func GetFX(ctx context.Context, fromCurrency, toCurrency string, opts ...func(*fx.Options) error) (*fx.Data, error) {
